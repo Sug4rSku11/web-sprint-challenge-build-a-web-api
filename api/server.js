5021f105aebe8bc110a4e2628c529c6bdaea0fc8
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2021-12-02 20:22:25 -0700

    started code in package.json, index and server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const server = express();
-
 // Configure your server here
+const server = express();
+server.use(express.json())
 // Build your actions router in /api/actions/actions-router.js
+const actionsRouter = require('./actions/actions-router')
 // Build your projects router in /api/projects/projects-router.js
+const projectsRouter = require('./projects/projects-router')
 // Do NOT `server.listen()` inside this file!
 
 module.exports = server;
